4ecc182a47cd6fe2ef5599ea40e22d969198bc97
Daniela Silva <145657442+danielafgsilva@users.noreply.github.com>
2025-07-19 16:15:38 +0100

    Feat: Get Devs by Terms

--- src/routes/devsRoutes.js
+++ src/routes/devsRoutes.js
@@ -1,9 +1,14 @@
 import express from 'express';
-import { getDevById, getDevs } from '../controllers/devsController.js';
+import { getDevById, getDevs, getDevsByTerms } from '../controllers/devsController.js';
 
 const router = express.Router();
 
-router.get('/', getDevs);
+router.get("/", (req, res) => {
+  if (req.query.terms !== undefined) {
+    return getDevsByTerms(req, res);
+  }
+  return getDevs(req, res);
+});
 
 router.get('/:id', getDevById)
 
