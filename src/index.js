91e632938681df6b1f4f7d2491213b2115f317b6
Daniela Silva <145657442+danielafgsilva@users.noreply.github.com>
2025-07-19 17:32:56 +0100

    Chore: add cors validation to enable frontend requests

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import devsRoutes from './routes/devsRoutes.js';
+import express from "express";
+import dotenv from "dotenv";
+import devsRoutes from "./routes/devsRoutes.js";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+app.use(cors());
 app.use(express.json());
-app.use('/devs', devsRoutes);
+app.use("/devs", devsRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
